17ac874fac8d76af1f11fae1f17d00eb0e9a8305
Aimee Pierce <Aimee Pierce>
2016-04-09 03:20:53 -0500

    extension completed

--- extension/popup.js
+++ extension/popup.js
@@ -1,19 +1,50 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
-
+  var Button = document.getElementById('Submit');
+  
+  Button.addEventListener('click', function() {
+	
+	var genderAuth;
+	var genderA = document.getElementsByClassName('genderA');
+	for(var i = 0; i < genderA.length; i++){
+	  if(genderA[i].checked){
+		  genderAuth = genderA[i].value;
+	  }
+	}
+	var genderSub;
+	var genderS = document.getElementsByClassName('genderS');
+	for(var i = 0; i < genderS.length; i++){
+	  if(genderS[i].checked){
+		  genderSub = genderS[i].value;
+	  }
+	}
+	
     chrome.tabs.getSelected(null, function(tab) {
       d = document;
-
       var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
+	  
+	  var gA = d.createElement('input');
+	  gA.name = 'author';
+	  gA.type = 'hidden';
+	  gA.value = genderAuth;
+	  
+	  var gS = d.createElement('input');
+	  gS.name = 'subject';
+	  gS.type = 'hidden';
+	  gS.value = genderSub;
+	  
+	  var i = d.createElement('input');
+      i.name = 'text';
+	  i.type = 'hidden';
       i.value = tab.url;
+	  
+	  
+	  f.appendChild(gA);
+	  f.appendChild(gS);
       f.appendChild(i);
       d.body.appendChild(f);
+	  
+	  f.action = 'https://hesaidshesaid.herokuapp.com/learn';
+      f.method = 'post';
       f.submit();
     });
   }, false);
