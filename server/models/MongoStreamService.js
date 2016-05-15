137600a8aa60bc715553f9c8b1846f698f848339
garret.meier <garret.meier@bunchball.com>
2016-05-15 16:11:01 -0500

    updating style

--- server/models/MongoStreamService.js
+++ server/models/MongoStreamService.js
@@ -2,7 +2,7 @@ var MongoClient = require("mongodb").MongoClient;
 var ObjectId = require("mongodb").ObjectID;
 var request = require("request");
 
-var mongo_url = process.env["MONGOLAB_URI"] || "mongodb://heroku_7x8bk4nr:a6bmbkcih22skroj9f33hlfl25@ds021010.mlab.com:21010/heroku_7x8bk4nr";
+var mongoUrl = process.env["MONGOLAB_URI"] || "mongodb://heroku_7x8bk4nr:a6bmbkcih22skroj9f33hlfl25@ds021010.mlab.com:21010/heroku_7x8bk4nr";
 
 function MongoStreamService(){
     this.stream = new Array();
@@ -15,7 +15,7 @@ MongoStreamService.getStreamItem = function(type,update,identifier){
     }
 
     return {type:type,update:update,id:identifier};
-}
+};
 
 MongoStreamService.prototype.commit = function(){
     //commit stuff in the stream
@@ -26,27 +26,27 @@ MongoStreamService.prototype.commit = function(){
     this.updating = true;
     var self = this;
 
-    MongoClient.connect(mongo_url,function(err,db){
+    MongoClient.connect(mongoUrl,function(err,db){
         if (err){
-            console.log(err);
+            //console.log(err);
             return ;
         }
 
         self.commitDB(db);
     });
-}
+};
 
 MongoStreamService.prototype.commitDB = function(db){
     var popped = this.stream.shift();
     var self = this;
-    var collection_name = popped.type;
+    var name = popped.type;
     var query = {name:-1};
 
     if (popped.id){
         query = popped.id;
     }
 
-    db.collection(collection_name).update(query,popped.update,{upsert:true},function(err,results){
+    db.collection(name).update(query,popped.update,{upsert:true},function(err,results){
         if (self.stream && self.stream.length > 0){
             self.commitDB(db);
         }else{
@@ -54,51 +54,51 @@ MongoStreamService.prototype.commitDB = function(db){
             db.close();
         }
     });
-}
+};
 
 MongoStreamService.prototype.push = function(item){
     this.merge(item);
-    this.commit()
+    this.commit();
 }
 
 MongoStreamService.prototype.merge = function(item){
     var mp = this.getMergePosition(item.type);
 
-        this.stream.push(item);
-        return;
+    this.stream.push(item);
+    return;
 /*    }
 
     var update_keys = Object.keys(item.update);
 
     for (i=0;i<update_keys.length;i++){
         var key = update_keys[i];
-        var update_item = item.update[key];
+        var updateItem = item.update[key];
 
         if (this.stream[mp].update[key]){
-            update_item = this.getUpdateItem(mp,key,item.update[key]);
+            updateItem = this.getUpdateItem(mp,key,item.update[key]);
         }
 
-        this.stream[mp].update[key] = update_item;
+        this.stream[mp].update[key] = updateItem;
     }*/
 
 }
 
-MongoStreamService.prototype.getUpdateItem = function (merge_position,key,update_item){
-    var new_item = {};
-    var cur_keys = Object.keys(this.stream[merge_position].update[key]);
+MongoStreamService.prototype.getUpdateItem = function (mergePosition,key,updateItem){
+    var newItem = {};
+    var curKeys = Object.keys(this.stream[mergePosition].update[key]);
 
     // copy existing item
-    for (i=0;i<cur_keys.length;i++){
-        new_item[cur_keys[i]] = this.stream[merge_position].update[key][cur_keys[i]];
+    for (var i=0;i<curKeys.length;i++){
+        newItem[curKeys[i]] = this.stream[mergePosition].update[key][curKeys[i]];
     }
 
     //add new values
-    var new_keys = Object.keys(update_item);
-    for (i=0;i<new_keys.length;i++){
-        if (new_item[new_keys[i]] && typeof new_item[new_keys[i]] === "number"){
-            new_item[new_keys[i]] += update_item[new_keys[i]];
+    var newKeys = Object.keys(updateItem);
+    for (var i=0;i<newKeys.length;i++){
+        if (newItem[newKeys[i]] && typeof newItem[newKeys[i]] === "number"){
+            newItem[newKeys[i]] += updateItem[newKeys[i]];
         }else{
-            new_item[new_keys[i]] = update_item[new_keys[i]];
+            newItem[newKeys[i]] = updateItem[newKeys[i]];
         }
     }
 
